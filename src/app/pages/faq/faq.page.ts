685780c84adcc4e7d13108b3a80bca77a7c188f1
AndreasGassmann <andreas@andreasgassmann.ch>
2020-05-30 17:59:30 +0000

    feat(faq): update faq page

--- src/app/pages/faq/faq.page.ts
+++ src/app/pages/faq/faq.page.ts
@@ -19,6 +19,7 @@ interface Faq {
 	styleUrls: ['./faq.page.scss']
 })
 export class FaqPage {
+	public searchTerm: string = '';
 	public faqs: Faq[] = [
 		{
 			question: 'What is the InviteManager bot?',
@@ -71,6 +72,21 @@ export class FaqPage {
 			group: FaqGroup.PRO_BOT
 		},
 		{
+			question: 'Can I pay for premium with Bitcoin or Ethereum?',
+			answer: 'Answer...',
+			group: FaqGroup.PRO_BOT
+		},
+		{
+			question: 'Can I pay for premium with Paypal?',
+			answer: 'Answer...',
+			group: FaqGroup.PRO_BOT
+		},
+		{
+			question: 'Do you offer a lifetime payment instead of a monthly payment?',
+			answer: 'Answer...',
+			group: FaqGroup.PRO_BOT
+		},
+		{
 			question: 'I bought the custom bot, how can I get it?',
 			answer: 'Answer...',
 			group: FaqGroup.CUSTOM_BOT
@@ -96,11 +112,32 @@ export class FaqPage {
 			group: FaqGroup.CUSTOM_BOT
 		},
 		{
+			question: 'I lost access to my old account and I would like to have my bot token back. What can I do?',
+			answer: `We're sorry you lost access to your account. For security reasons, we will <strong>never</strong> give out a bot token that has been given to us. We handle the tokens very carefully and make sure nobody gets access to them. We use them only for the purpose of running our bot. If you might need your bot token at a later time, please make a backup yourself.`,
+			group: FaqGroup.CUSTOM_BOT
+		},
+		{
 			question: 'How can I control which servers can use my custom bot?',
 			answer: 'Answer...',
 			group: FaqGroup.CUSTOM_BOT
 		}
 	];
 
+	public filteredFaqs: Faq[] = this.faqs;
+
 	constructor() {}
+
+	public async applyFilter(event?: { target?: { value?: string } }): Promise<void> {
+		const searchTerm: string | undefined = event?.target?.value;
+		if (searchTerm) {
+			const lowerCaseSearchTerm: string = searchTerm.toLowerCase();
+			this.filteredFaqs = this.faqs.filter(
+				(faq) =>
+					faq.question.toLowerCase().includes(lowerCaseSearchTerm) ||
+					faq.answer.toLowerCase().includes(lowerCaseSearchTerm)
+			);
+		} else {
+			this.filteredFaqs = this.faqs;
+		}
+	}
 }
