8f9bb34fa0e05d9954bf5508de788ce4a045e0c7
AndreasGassmann <andreas@andreasgassmann.ch>
2020-06-07 03:22:32 +0200

    fix(e2e): add text

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('new App', () => {
+describe('InviteManager', () => {
 	let page: AppPage;
 
 	beforeEach(() => {
@@ -8,10 +8,10 @@ describe('new App', () => {
 	});
 	describe('default screen', () => {
 		beforeEach(() => {
-			page.navigateTo('/Inbox');
+			page.navigateTo('/');
 		});
-		it('should say Inbox', () => {
-			expect(page.getParagraphText()).toContain('Inbox');
+		it('should contain text', () => {
+			expect(page.getParagraphText()).toContain('Commands');
 		});
 	});
 });
